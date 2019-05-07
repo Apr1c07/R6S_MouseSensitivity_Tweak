ce25568f6d5f006f21c4433c98f53579d9ca8511
Apr1c07 <40536719+Apr1c07@users.noreply.github.com>
2019-05-07 22:32:24 +0900

    Update Cal.js

--- js/Cal.js
+++ js/Cal.js
@@ -1,5 +1,5 @@
 alert('読み込み成功');
 
 function onButtonClick() {
-  form1.XFA.innerText=(from1.DPI.value + form1.x.value + form1.y.value+form1.ADS.value+form1.MSMU.value);
+  form1.Result.value=(from1.DPI.value + form1.x.value + form1.y.value+form1.ADS.value+form1.MSMU.value+form1.XFA.value);
 }
