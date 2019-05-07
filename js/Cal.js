5e495859f56d7763806c7c3a5de24da4d3691fce
Apr1c07 <40536719+Apr1c07@users.noreply.github.com>
2019-05-07 21:58:56 +0900

    Update Cal.js

--- js/Cal.js
+++ js/Cal.js
@@ -1,3 +1,5 @@
+document.write("<p>UTF-8で記述された外部ファイル</p>");
+
 function onButtonClick() {
 　alert('JavaScriptのアラート');
   form1.XFA.value=(from1.DPI.value + form1.x.value + form1.y.value+form1.ADS.value+form1.MSMU.value);
