08d917db3af4a025cb117ee649dd5059d36b52c0
Apr1c07 <40536719+Apr1c07@users.noreply.github.com>
2019-05-07 21:41:13 +0900

    Add files via upload

---
+++ Cal.js
@@ -0,0 +1,7 @@
+function onButtonClick() {
+  target = document.getElementById("XFA");
+  target.innerText =(document.getElementById('x')+
+  document.getElementById('y')+
+  document.getElementById('ADS')+
+  document.getElementById('MSMU'));
+}
