06a4e6633e8602069e93a3004b03802cf51539f7
Apr1c07 <40536719+Apr1c07@users.noreply.github.com>
2019-05-07 23:25:55 +0900

    Update Cal.js

--- js/Cal.js
+++ js/Cal.js
@@ -1,5 +1,10 @@
 function onButtonClick() {
-  alert('計算します');
-  form1.Result.value=(from1.DPI.value + form1.x.value + form1.y.value+form1.ADS.value+form1.MSMU.value+form1.XFA.value);
-  alert(form1.Result.value);
+  var DPI=Number(document.getElementById('DPI').value);
+  var x=Number(document.getElementById('x').value);
+  var y=Number(document.getElementById('y').value);
+  var ADS =Number(document.getElementById('ADS').value);
+  var MSMU=Number(document.getElementById('MSMU').value);
+  var XFA =Number(document.getElementById('XFA').value);
+  var obj = document.getElementById('Result');
+  obj.value=DPI+x+y+ADS+MSMU+XFA;
 }
